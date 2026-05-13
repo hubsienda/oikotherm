07b99ff57313a3724685caba4cfe99c7fbaa0683
hubsienda <hub@siendaweblines.com>
2026-05-13 17:37:00 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Calculator from '@/components/Calculator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-stone-50 px-4 py-6 text-stone-950 sm:px-6 lg:px-8">
+      <Calculator />
+    </main>
+  );
+}
